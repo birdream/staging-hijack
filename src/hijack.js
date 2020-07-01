176256d4ea6a96ac83d2e8cec831bafbd0223470
Norman Chen <norman@NormandeMacBook-Pro.local>
2020-07-01 14:04:19 +0800

    hijack

--- src/hijack.js
+++ src/hijack.js
@@ -2,14 +2,21 @@ const rp = require('request-promise');
 
 module.exports = async (req, res) => {
 
-  // await rp({
-  //     method: 'POST',
-  //     uri: 'https://chat.shoplinestg.com/webhook',
-  //     body: req.body,
-  //     json: true
-  // })
-
+  try {
+    console.log('--sending..')
+    await rp({
+      method: 'POST',
+      uri: 'https://chat.shoplinestg.com/webhook',
+      body: req.body,
+      json: true
+   })
+   console.log('----send to chat succeed.')
+  } catch (error) {
+    console.log('send to cha fail', error.message || error)
+  }
+  
   console.log('--------------------------------------------');
+  console.dir(req.query, {depth: 9});
   console.dir(req.body, {depth: 9});
 
   return res.send('ok.')
