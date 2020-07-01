27983df75299c7ff1e0b42ae8405ad544baa3c93
Norman Chen <norman@NormandeMacBook-Pro.local>
2020-07-01 14:00:30 +0800

    hijack

--- src/hijack.js
+++ src/hijack.js
@@ -2,12 +2,12 @@ const rp = require('request-promise');
 
 module.exports = async (req, res) => {
 
-  await rp({
-      method: 'POST',
-      uri: 'https://chat.shoplinestg.com/webhook',
-      body: req.body,
-      json: true
-  })
+  // await rp({
+  //     method: 'POST',
+  //     uri: 'https://chat.shoplinestg.com/webhook',
+  //     body: req.body,
+  //     json: true
+  // })
 
   console.log('--------------------------------------------');
   console.dir(req.body, {depth: 9});
