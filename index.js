eb9c750f5bbf110e572d4c603977aa1a20f6c385
Norman Chen <norman@NormandeMacBook-Pro.local>
2020-07-07 15:21:47 +0800

    html

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const port = process.env.PORT || 3000
 const validate = require('./src/validate')
 const hijack = require('./src/hijack')
 
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    res.sendFile(`${__dirname}/src/pages/index.html`)
+})
+
+app.use('/web', router)
 app.use(bodyParser.json())
 app.get('/', (req, res) => res.send('ok..'))
 app.get('/favicon.ico', (req, res) => res.send('ok...'))
