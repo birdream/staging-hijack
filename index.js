a2ad74c5e058b05a06f0cd71ad4e48faa0df50d0
birdream <bird.dream.norman@gmail.com>
2020-07-09 11:16:36 +0800

    ig

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000
 
 const validate = require('./src/validate')
 const hijack = require('./src/hijack')
+const live = require('./src/instagram');
 
 const router = express.Router();
 
@@ -19,5 +20,11 @@ app.get('/favicon.ico', (req, res) => res.send('ok...'))
 
 app.get('/webhook', validate)
 app.post('/webhook', hijack)
+
+app.post('/ig/login', live.login)
+app.get('/ig/live/prepare', live.prepareLive)
+app.get('/ig/live/start', live.startLive)
+app.get('/ig/live/end', live.endLive)
+// app.get('/webhook', live.)
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
